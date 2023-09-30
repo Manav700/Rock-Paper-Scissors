7f6e4893f0b9db8baad89eba6351adb213e2b147
Manav <manavwork07@gmail.com>
2023-09-30 12:17:18 +0530

    Adding Functionalities

--- app.js
+++ app.js
@@ -1,82 +1,93 @@
-let playerscore=0;
-let computerScore=0;
+let playerscore = 0;
+let computerScore = 0;
 let roundsPlayed = 0;
+let playerSelection;
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      console.log("It's a Draw my friend");
-      playerscore++;
-      computerScore++;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-      console.log("Computer Won!!");
-      computerScore++;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      console.log("Player Won!!");
-      playerscore++;
-    }
-     else if (playerSelection === "paper" && computerSelection === "scissors") {
-      console.log("Computer Won!!");
-      computerScore++;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      console.log("Player Won!!");
-      playerscore++;
+    const computerButtons = document.querySelectorAll(".buttonforcomputer button"); 
     
+    // Remove  "selected" class from all computer buttons
+    computerButtons.forEach((button) => {
+        button.classList.remove("selected");
+    });
+
+    // Add "selected" class to the computer's choice button
+    const computerSelectedButton = document.querySelector(`.buttonforcomputer .${computerSelection}`);
+    // computerSelectedButton.classList.add("selected");
+
+    if (playerSelection === computerSelection) {
+        playerscore++;
+        computerScore++;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerscore++;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerscore++;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-      console.log("Computer Won!!");
-      computerScore++;
+        computerScore++;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      console.log("Player Won!!");
-      playerscore++;
+        playerscore++;
     }
+
     roundsPlayed++;
     updateScoreDisplay();
-    if (roundsPlayed === 5) {
-      document.getElementById("finalScore").style.display = "block";
-      document.getElementById("currentScore").style.display = "none"
-  }
 
-  }
-function getComputerChoice(){
-    let random = Math.floor(Math.random()*3) +1;
-    if (random == 1){
-       return "rock"
-       
-    }
-    if (random == 2){
-        return "paper"
-       
-    }
-    if (random == 3){
-       return "scissors"
-    }
-
+    setTimeout(() => {
+        computerSelectedButton.classList.remove("selected");
+    }, 300);
 }
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+        return "rock";
+    }
+    if (random === 2) {
+        return "paper";
+    }
+    if (random === 3) {
+        return "scissors";
+    }
+}
+
 function updateScoreDisplay() {
-  const currentScoreElement = document.getElementById("currentScore");
-  currentScoreElement.textContent = `Current Score: Player: ${playerscore} Computer: ${computerScore}`;
+    const currentScoreElement = document.getElementById("currentScore");
+    currentScoreElement.textContent = `Current Score: Player: ${playerscore} Computer: ${computerScore}`;
 }
 
-const rockButton = document.querySelector(".rock")
-const paperButton = document.querySelector(".paper")
-const sissorsButton = document.querySelector(".scissors")
-const crockButton = document.querySelector(".crock")
-const cpaperButton = document.querySelector(".cpaper")
-const csissorsButton = document.querySelector(".csissors")
-const displayScore= document.querySelector(".displayscore")
-const scoreValueElement = document.getElementById("scoreValue");
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+
 rockButton.addEventListener("click", () => {
-  let computerSelection = getComputerChoice();
-  playerSelection='rock'
-  playRound(playerSelection, computerSelection);
-  
+    let computerSelection = getComputerChoice();
+    playerSelection = 'rock';
+    playRound(playerSelection, computerSelection);
 });
+
 paperButton.addEventListener("click", () => {
-  let computerSelection = getComputerChoice();
-  playerSelection='paper'
-  playRound(playerSelection, computerSelection);
-  
+    let computerSelection = getComputerChoice();
+    playerSelection = 'paper';
+    playRound(playerSelection, computerSelection);
 });
-sissorsButton.addEventListener("click", () => {
-  let computerSelection = getComputerChoice();
-  playerSelection='scissors'
-  playRound(playerSelection, computerSelection);
+
+scissorsButton.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection);
 });
+
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+    playerscore = 0;
+    computerScore = 0;
+    roundsPlayed = 0;
+    updateScoreDisplay();
+    document.getElementById("finalScore");
+    document.getElementById("currentScore");
+}
